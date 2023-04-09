b4e319b1aa4b628ee899810733b11614682af91e
Monishacodes <Monishanarayan10@gmail.com>
2023-04-09 23:55:45 +0530

    Accordion build on home page.

---
+++ src/Components/Home/Accordion.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import Heading from '../shared/common/heading';
+import Paragraph from '../shared/common/paragraph';
+
+const Accordion1=()=> {
+  return (
+    <div className='container py-4 mb-5'>
+    <Heading title="Why AstroDevam?" />
+  <div className='container'>
+  <Paragraph paragraph="One of the best online Astrology platforms to connect with experienced and verified Astrologers" />
+  </div>
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Verified Astrologers</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Ask An Astrologer Via Multiple Ways</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>100% Privacy Guaranteed</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    
+    </div>
+  );
+}
+
+export default Accordion1;
--- src/Components/shared/common/heading.js
+++ src/Components/shared/common/heading.js
@@ -4,3 +4,4 @@ const Heading=({title})=> {
   return <div className="text-center py-4"> <h2 className='heading pb-3'> {title} </h2> </div>
 }
 export default Heading;
+  
