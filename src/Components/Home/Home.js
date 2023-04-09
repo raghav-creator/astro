8b529cf54c8c294b8a894a0469a3e334c73e04ab
raghav vohra <raghavvohra9gmail.com>
2023-04-09 20:32:49 +0530

    Added astrologr section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Header from './../shared/header';
 import Footer from './../shared/footer';
 import Banner from './banner';
+import Astrologer from './astrologer';
+import AstrologerList from './astrologerlist';
+
 const Home=()=>
 {
   return <div> <Header />
   <Banner />
+  <Astrologer />
+  <AstrologerList />
   <Footer />
   </div>;
 }
